7eac28731bb27856ea728d06fb8eb25af3c31b1a
Dpuk123 <padipu9@gmail.com>
2023-06-15 11:06:16 +0700

    Initial commit

---
+++ app/src/main/java/com/paulinusdianputra_202102354/forex/RatesModel.java
@@ -0,0 +1,37 @@
+package com.paulinusdianputra_202102354.forex;
+
+public class RatesModel
+{
+    private double IDR, MYR, SGD, THB, AED, AFN, ALL, ANG, JPY, AUD;
+    public RatesModel() {}
+
+    public double getAUD() {return IDR; }
+    public void setAUD(double AUD) { this.IDR = AUD; }
+
+    public double getBND() {return MYR; }
+    public void setBND(double BND) { this.MYR = BND; }
+
+    public double getBTC() {return SGD; }
+    public void setBTC(double BTC) { this.SGD = BTC; }
+
+    public double getEUR() {return THB; }
+    public void setEUR(double EUR) { this.THB = EUR; }
+
+    public double getGBP() {return AED; }
+    public void setGBP(double GBP) { this.AED = GBP; }
+
+    public double getHKD() {return AFN; }
+    public void setHKD(double HKD) { this.AFN = HKD; }
+
+    public double getIDR() {return ALL; }
+    public void setIDR(double IDR) { this.ALL = IDR; }
+
+    public double getINR() {return ANG; }
+    public void setINR(double INR) { this.ANG = INR; }
+
+    public double getJPY() {return JPY; }
+    public void setJPY(double JPY) { this.JPY = JPY; }
+
+    public double getMYR() {return AUD ; }
+    public void setMYR(double MYR) { this.AUD = MYR; }
+}
